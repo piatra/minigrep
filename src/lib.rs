7b99dc5b0d2c1a01a2c19904119a513345f46acb
Andrei Oprea <andrei.br92@gmail.com>
2018-07-30 22:50:01 +0200

    Implement case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,36 @@ mod test {
             assert!(true);
         }
     }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+            );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+            );
+    }
 }
 
 pub struct Config {
@@ -50,5 +80,20 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents.split("\n").filter(|line| {
+        line.to_lowercase().contains(&query)
+    }).collect()
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.split("\n").filter(|line| line.contains(query)).collect()
 }
